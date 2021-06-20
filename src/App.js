64bdddff062fb883a5317ee73de94a4cc7fc5a93
cihat <cihat02020202@gmail.com>
2021-06-20 19:20:22 +0300

    Add tbody elements in tables on App.js

--- src/App.js
+++ src/App.js
@@ -38,143 +38,149 @@ class App extends React.Component {
           <hr />
           <div>
             <table>
-              <tr>
-                <td style={{ width: '150px' }}>Kasadaki para:</td>
-                <td>{this.game.money}₺</td>
-              </tr>
-              <tr>
-                <td>Dolaptaki adet:</td>
-                <td>{this.game.currentCigKofte}</td>
-              </tr>
-              <tr>
-                <td>Fiyat:</td>
-                <td>
-                  {this.game.price}₺
-                  <button
-                    disabled={!this.game.canDecreasePrice()}
-                    style={{ marginLeft: '20px' }}
-                    onClick={this.game.decreasePrice}
-                  >
-                    -
-                  </button>
-                  <button
-                    style={{ marginLeft: '10px' }}
-                    onClick={this.game.increasePrice}
-                  >
-                    +
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Halkın talebi:</td>
-                <td>%{this.game.demandRate}</td>
-              </tr>
+              <tbody>
+                <tr>
+                  <td style={{ width: '150px' }}>Kasadaki para:</td>
+                  <td>{this.game.money}₺</td>
+                </tr>
+                <tr>
+                  <td>Dolaptaki adet:</td>
+                  <td>{this.game.currentCigKofte}</td>
+                </tr>
+                <tr>
+                  <td>Fiyat:</td>
+                  <td>
+                    {this.game.price}₺
+                    <button
+                      disabled={!this.game.canDecreasePrice()}
+                      style={{ marginLeft: '20px' }}
+                      onClick={this.game.decreasePrice}
+                    >
+                      -
+                    </button>
+                    <button
+                      style={{ marginLeft: '10px' }}
+                      onClick={this.game.increasePrice}
+                    >
+                      +
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Halkın talebi:</td>
+                  <td>%{this.game.demandRate}</td>
+                </tr>
+              </tbody>
             </table>
           </div>
           <div style={{ marginTop: '16px' }}>
             <div>Üretim</div>
             <hr />
             <table>
-              <tr>
-                <td style={{ width: '150px' }}>Çiğ köfte / dakika:</td>
-                <td>{this.game.lastManufacturedRate}</td>
-              </tr>
-              <tr>
-                <td>Malzeme:</td>
-                <td>
-                  {this.game.material} gr
-                  <button
-                    style={{ marginLeft: '10px' }}
-                    disabled={!this.game.canBuyMaterial()}
-                    onClick={this.game.buyMaterial}
-                  >
-                    Satin Al! ({this.game.materialCost}₺)
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Satınalma müdürü:</td>
-                <td>
-                  {this.game.hasAutoBuyer ? (
-                    <React.Fragment>
-                      {this.game.isAutoBuyerActive ? 'Aktif' : 'Durdu'}
-                      <button
-                        style={{ marginLeft: '10px' }}
-                        onClick={
-                          this.game.isAutoBuyerActive
-                            ? this.game.stopAutoBuyer
-                            : this.game.startAutoBuyer
-                        }
-                      >
-                        {this.game.isAutoBuyerActive ? 'Durdur' : 'Devam et'}
-                      </button>
-                    </React.Fragment>
-                  ) : (
-                    <span>
-                      Yok
-                      {this.game.didUnlockAutoBuyer() && (
+              <tbody>
+                <tr>
+                  <td style={{ width: '150px' }}>Çiğ köfte / dakika:</td>
+                  <td>{this.game.lastManufacturedRate}</td>
+                </tr>
+                <tr>
+                  <td>Malzeme:</td>
+                  <td>
+                    {this.game.material} gr
+                    <button
+                      style={{ marginLeft: '10px' }}
+                      disabled={!this.game.canBuyMaterial()}
+                      onClick={this.game.buyMaterial}
+                    >
+                      Satin Al! ({this.game.materialCost}₺)
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Satınalma müdürü:</td>
+                  <td>
+                    {this.game.hasAutoBuyer ? (
+                      <React.Fragment>
+                        {this.game.isAutoBuyerActive ? 'Aktif' : 'Durdu'}
                         <button
                           style={{ marginLeft: '10px' }}
-                          disabled={!this.game.canBuyAutoBuyer()}
-                          onClick={this.game.buyAutoBuyer}
+                          onClick={
+                            this.game.isAutoBuyerActive
+                              ? this.game.stopAutoBuyer
+                              : this.game.startAutoBuyer
+                          }
                         >
-                          Satin Al! ({this.game.autoBuyerCost}₺)
+                          {this.game.isAutoBuyerActive ? 'Durdur' : 'Devam et'}
                         </button>
-                      )}
-                    </span>
-                  )}
-                </td>
-              </tr>
+                      </React.Fragment>
+                    ) : (
+                      <span>
+                        Yok
+                        {this.game.didUnlockAutoBuyer() && (
+                          <button
+                            style={{ marginLeft: '10px' }}
+                            disabled={!this.game.canBuyAutoBuyer()}
+                            onClick={this.game.buyAutoBuyer}
+                          >
+                            Satin Al! ({this.game.autoBuyerCost}₺)
+                          </button>
+                        )}
+                      </span>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
             </table>
             <div style={{ marginTop: '16px' }}>
               <div>Çalışan:</div>
               <hr />
               <table>
-                <tr>
-                  <td>Çırak:</td>
-                  <td style={{ width: '50px', textAlign: 'center' }}>
-                    {this.game.autoGenerators.errandBoy}
-                  </td>
-                  <td>
-                    <button
-                      style={{ marginLeft: '10px' }}
-                      disabled={!this.game.canBuyAutoGenerator('ERRAND_BOY')}
-                      onClick={() => this.game.buyAutoGenerator('ERRAND_BOY')}
-                    >
-                      Satın Al! ({this.game.autoGenerators.errandBoyCost}₺)
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Kalfa:</td>
-                  <td style={{ width: '50px', textAlign: 'center' }}>
-                    {this.game.autoGenerators.foreman}
-                  </td>
-                  <td>
-                    <button
-                      style={{ marginLeft: '10px' }}
-                      disabled={!this.game.canBuyAutoGenerator('FOREMAN')}
-                      onClick={() => this.game.buyAutoGenerator('FOREMAN')}
-                    >
-                      Satın Al! ({this.game.autoGenerators.foremanCost}₺)
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Usta:</td>
-                  <td style={{ width: '50px', textAlign: 'center' }}>
-                    {this.game.autoGenerators.master}
-                  </td>
-                  <td>
-                    <button
-                      style={{ marginLeft: '10px' }}
-                      disabled={!this.game.canBuyAutoGenerator('MASTER')}
-                      onClick={() => this.game.buyAutoGenerator('MASTER')}
-                    >
-                      Satın Al! ({this.game.autoGenerators.masterCost}₺)
-                    </button>
-                  </td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <td>Çırak:</td>
+                    <td style={{ width: '50px', textAlign: 'center' }}>
+                      {this.game.autoGenerators.errandBoy}
+                    </td>
+                    <td>
+                      <button
+                        style={{ marginLeft: '10px' }}
+                        disabled={!this.game.canBuyAutoGenerator('ERRAND_BOY')}
+                        onClick={() => this.game.buyAutoGenerator('ERRAND_BOY')}
+                      >
+                        Satın Al! ({this.game.autoGenerators.errandBoyCost}₺)
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Kalfa:</td>
+                    <td style={{ width: '50px', textAlign: 'center' }}>
+                      {this.game.autoGenerators.foreman}
+                    </td>
+                    <td>
+                      <button
+                        style={{ marginLeft: '10px' }}
+                        disabled={!this.game.canBuyAutoGenerator('FOREMAN')}
+                        onClick={() => this.game.buyAutoGenerator('FOREMAN')}
+                      >
+                        Satın Al! ({this.game.autoGenerators.foremanCost}₺)
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Usta:</td>
+                    <td style={{ width: '50px', textAlign: 'center' }}>
+                      {this.game.autoGenerators.master}
+                    </td>
+                    <td>
+                      <button
+                        style={{ marginLeft: '10px' }}
+                        disabled={!this.game.canBuyAutoGenerator('MASTER')}
+                        onClick={() => this.game.buyAutoGenerator('MASTER')}
+                      >
+                        Satın Al! ({this.game.autoGenerators.masterCost}₺)
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </div>
